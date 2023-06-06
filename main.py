e935e1d2073f9524f863347ebe97ce9b43e4f7b7
Elsa Ko <eko095@uottawa.ca>
2023-06-06 14:21:02 +0000

    Dynamic Sizing (Home), Navigation

--- main.py
+++ main.py
@@ -36,6 +36,22 @@ def apply():
     else:
         return render_template('index.html')
 
+@app.route('/model')
+def model():
+    return render_template('model.html')
+    
+@app.route('/about')
+def about():
+    return render_template('about.html')
+    
+@app.route('/history')
+def history():
+    return render_template('history.html')
+    
+@app.route('/visualize', methods=['GET'])
+def visualize():
+    return render_template('visualize.html')
+
 if __name__=="__main__":
     app.run(debug=True)
     
